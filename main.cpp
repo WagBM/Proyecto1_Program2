20ad6eda3d906b5abfdcfc6a8334bb778f6896ed
WagBM <wagnerbarrantes12@gmail.com>
2023-05-08 00:14:31 -0600

    Se agregra un comentario

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
-/*
- * 
- */
+//hola mundo
+
 int main(int argc, char** argv) {
 
     return 0;
